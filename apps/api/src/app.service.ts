a6090c4b8198c3414029b3943f3216a82b85d834
Juan Frank <juan.frank@gm2dev.com>
2024-12-09 15:40:33 -0300

    feat: create counter service

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@nestjs/common';
-
+import * as fs from 'fs';
+import * as path from 'path';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  private readonly filePath = path.resolve(__dirname, '../src/db.json');
+
+  private readJson(): { count: number } {
+    const data = fs.readFileSync(this.filePath, 'utf8');
+    return JSON.parse(data);
+  }
+
+  private writeJson(data: { count: number }): void {
+    fs.writeFileSync(this.filePath, JSON.stringify(data, null, 2), 'utf8');
+  }
+
+  getHello(): number {
+    const data = this.readJson();
+    return data.count
+  }
+
+  sum() {
+    const data = this.readJson();
+    data.count += 1;
+    this.writeJson(data);
+    return data.count;
+  }
+
+  res() {
+    const data = this.readJson();
+    data.count -= 1;    
+    this.writeJson(data);
+    return data.count;
   }
 }
